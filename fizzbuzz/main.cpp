bcaded588fa7a3121c987195470ee4a287b741c7
eejackliu <pipiluliu@sina.com>
2017-03-09 15:07:26 +0800

    fizzbuzz ok

---
+++ fizzbuzz/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+using namespace std;
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+                vector<string> out;string temp;
+                   for(int i=1;i<=n;i++)
+                   {
+                       if((!(i%3))&&(i%5))
+                           out.push_back("Fizz");
+                       else if((i%3)&&(!(i%5)))
+                           out.push_back("Buzz");
+                       else if((!(i%3))&&(!(i%5)))
+                           out.push_back("FizeBuzz");
+                       else
+                       {
+                           stringstream ss;
+                           ss<<i;
+                           temp=ss.str();
+                           out.push_back(temp);
+                       }
+                   }
+                   return out;
+    }
+};
+
+
+int main(int argc, char *argv[])
+{
+    Solution a;
+    vector<string> tmp=a.fizzBuzz(12);
+    for(auto c:tmp)
+        cout<<c<<endl;
+    return 0;
+}
